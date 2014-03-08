74fa6aba2e7070fddec793efbb44ef0e55a8061e
guihehans <guihehans@gmail.com>
2014-03-08 21:37:19 +1100

    api all finished

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+Note.delete_all
+
+user1 = User.create({name:'guihehans', password: 'password'})
+user2 = User.create({name:'notguihe', password: '123456'})
+
+Note.create(user:user1, title: 'my first note')
+Note.create(user:user1, title: 'my 2nd note')
+Note.create(user:user2, title: 'ahh, my first note')
+Note.create(user:user2, title: 'ohh, my 2nd note')
