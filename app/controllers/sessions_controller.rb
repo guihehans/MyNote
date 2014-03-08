ee889ccf6ce2f86cb055994be030e9d5537ec7ac
guihehans <guihehans@gmail.com>
2014-03-08 23:00:13 +1100

    README improved

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,13 +7,11 @@ class SessionsController < ApplicationController
   def create
       @user = User.find_by(name: params[:name])
       if @user.password == params[:password]
-        # Sign the user in and redirect to the user's show page.
         sign_in @user
         render :json => {token:@user.remember_token}.to_json
         return
       end
       else
-        # Create an error message and re-render the signin form.
         render :nothing => true, :status => 401
       end
   end
