b91cb5f15a0230aa198b18a7ff8dd892834210f4
guihehans <guihehans@gmail.com>
2014-03-08 20:49:50 +1100

    routes fix notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 MyNote::Application.routes.draw do
-  resources :notes
+
 
   resources :users
 
   resources :sessions, only: [:new, :create, :destroy]
-
+  get '/notes', to: 'notes#index', as: 'notes'
   post '/login', to: 'sessions#create', as: 'login'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
